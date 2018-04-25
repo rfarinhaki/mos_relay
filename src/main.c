a9145cea15288fc3d8ecc5a5cb108f9d113ad5aa
Ricardo Farinhaki <ricardo.farinhaki@polilinux.com.br>
2018-04-25 09:27:40 -0300

    Testing

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include "mgos.h"
 
+static void led_timer_cb(void *arg) { 
+    bool val = mgos_gpio_toggle(mgos_sys_config_get_pins_led());
+    LOG(LL_INFO, ("%s uptime: %.2lf, RAM: %lu, %lu free", val ? "Tick" : "Tock",
+                mgos_uptime(), (unsigned long) mgos_get_heap_size(),
+                (unsigned long) mgos_get_free_heap_size()));
+    (void) arg;
+}
+
 enum mgos_app_init_result mgos_app_init(void) {
   return MGOS_APP_INIT_SUCCESS;
 }
